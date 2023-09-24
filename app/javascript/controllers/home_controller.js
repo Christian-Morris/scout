900c0b5f5fe5504d4fe63b4a8e72566d41a2be22
Christian Morris <cjmorris96@hotmail.com>
2023-09-24 07:50:18 +0100

    disabled scroll feature on home page

--- app/javascript/controllers/home_controller.js
+++ app/javascript/controllers/home_controller.js
@@ -2,11 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="home"
 export default class extends Controller {
-  // connect() {
-  //   console.log("home controller connected :D")
-  // }
+  connect() {
+    window.scrollTo(0, 0);
+  }
 
   scroll () {
     window.scrollTo(0,document.body.scrollHeight);
+  }
+
+  cancelScroll (event) {
+    event.preventDefault();
   }
 }
